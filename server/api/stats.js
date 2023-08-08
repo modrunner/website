b597e3e09ab90f6f7c93bc9dd8fc2535155b1085
smcmo <shanemcmonigal@gmail.com>
2023-08-08 14:54:03 -0500

    Maybe this will do something

--- server/api/stats.js
+++ server/api/stats.js
@@ -7,21 +7,25 @@ export default defineEventHandler(async (event) => {
 	// 	.then((res) => res.json())
 	// 	.catch((error) => console.log('/server/api/stats.js -', error));
 
-	try {
-		const response = await fetch(`${useRuntimeConfig().apiUrl}/stats`, {
-			headers: {
-				'x-api-key': useRuntimeConfig().apiKey,
-			},
+	const response = await fetch(`${useRuntimeConfig().apiUrl}/stats`, {
+		headers: {
+			'x-api-key': useRuntimeConfig().apiKey,
+		},
+	}).catch((error) => {
+		throw createError({
+			statusMessage: 'Internal Server Error',
+			message: 'Request to the backend failed.',
 		});
+	});
 
-		console.log('Requested statistics from the backend:');
-		console.log(`- Status: ${response.status} ${response.statusText}`);
-		console.log(`- URL: ${response.url}`);
-
-		const data = await response.json();
-
-		return data;
-	} catch (error) {
-		console.log('/server/api/stats.js -', error);
+	if (response.status !== 200) {
+		throw createError({
+			statusCode: response.status,
+			statusMessage: response.statusText,
+			message:
+				'Request to the backend succeeded but returned a non-2xx status.',
+		});
 	}
+
+	return await response.json();
 });
