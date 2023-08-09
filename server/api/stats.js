76c2e2429eb31380bb7f66ccd272073baf27ba35
smcmo <shanemcmonigal@gmail.com>
2023-08-09 14:56:08 -0500

    Test2

--- server/api/stats.js
+++ server/api/stats.js
@@ -1,31 +1,9 @@
 export default defineEventHandler(async (event) => {
-	// return await fetch(`${useRuntimeConfig().apiUrl}/stats`, {
-	// 	headers: {
-	// 		'x-api-key': useRuntimeConfig().apiKey,
-	// 	},
-	// })
-	// 	.then((res) => res.json())
-	// 	.catch((error) => console.log('/server/api/stats.js -', error));
-
-	const response = await fetch(`${useRuntimeConfig().apiUrl}/stats`, {
+	return await fetch(`${useRuntimeConfig().apiUrl}/stats`, {
 		headers: {
 			'x-api-key': useRuntimeConfig().apiKey,
 		},
-	}).catch((error) => {
-		throw createError({
-			statusMessage: 'Internal Server Error',
-			message: 'Request to the backend failed.',
-		});
-	});
-
-	if (response.status !== 200) {
-		throw createError({
-			statusCode: response.status,
-			statusMessage: response.statusText,
-			message:
-				'Request to the backend succeeded but returned a non-2xx status.',
-		});
-	}
-
-	return await response.json();
+	})
+		.then(async (res) => await res.json())
+		.catch((error) => console.log('/server/api/stats.js -', error));
 });
