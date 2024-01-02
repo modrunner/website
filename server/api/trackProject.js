d7d7ddbb72a2ced85ee6feaf692265640f3d5b20
smcmo <shanemcmonigal@gmail.com>
2024-01-02 12:53:57 -0600

    Patch exploit in dashboard

--- server/api/trackProject.js
+++ server/api/trackProject.js
@@ -2,11 +2,15 @@ export default defineEventHandler(async (event) => {
 	const config = useRuntimeConfig()
 	const body = await readBody(event)
 
-	return await $fetch(`${config.apiUrl}/projects/track`, {
+	const res = await $fetch.raw(`${config.apiUrl}/projects/track`, {
 		method: 'POST',
 		headers: {
 			'x-api-key': config.apiKey,
 		},
 		body,
-	}).catch((error) => console.error('Error while sending an track request to the backend:', error))
+		ignoreResponseError: true,
+	})
+
+	setResponseStatus(event, res.status)
+	event.node.res.end()
 })
--- server/api/untrackProject.js
+++ server/api/untrackProject.js
@@ -2,11 +2,15 @@ export default defineEventHandler(async (event) => {
 	const config = useRuntimeConfig()
 	const body = await readBody(event)
 
-	return await $fetch(`${config.apiUrl}/projects/untrack`, {
+	const res = await $fetch.raw(`${config.apiUrl}/projects/untrack`, {
 		method: 'DELETE',
 		headers: {
 			'x-api-key': config.apiKey,
 		},
 		body,
-	}).catch((error) => console.error('Error while sending an untrack request to the backend:', error))
+		ignoreResponseError: true,
+	})
+
+	setResponseStatus(event, res.status)
+	event.node.res.end()
 })
